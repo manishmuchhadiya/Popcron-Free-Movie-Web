20e905acb46d1c1624999935e2e2df43269bb2b4
manishmuchhadiya <manishmuchhadiya8@gmail.com>
2024-06-07 17:23:39 +0530

    logout error has been sloved

--- movieWeb_project/movie_db.sql
+++ movieWeb_project/movie_db.sql
@@ -1,20 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
--- http://www.phpmyadmin.net
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 24, 2022 at 11:41 AM
--- Server version: 5.6.16
--- PHP Version: 5.5.11
+-- Generation Time: Jun 07, 2024 at 01:42 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `movie_db`
@@ -26,24 +27,26 @@ SET time_zone = "+00:00";
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `email` varchar(30) NOT NULL,
   `name` varchar(30) NOT NULL,
-  `pass` varchar(40) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+  `pass` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `email`, `name`, `pass`) VALUES
+(1, 'mihir@gmail.com', 'mihir', 'mmb'),
 (2, 'm@gmail.com', 'mihir', 'mmb'),
+(3, 'visal@gmail.com', 'visal', 'vmd'),
 (4, 'mm@gmail.com', 'manish', 'mmd'),
 (5, 'md@g.com', 'mmm', 'mmd'),
 (6, 'manish@gmail.com', 'manish', 'mmd'),
-(7, 'v@gmail.com', 'viraj', '123');
+(7, 'v@gmail.com', 'viraj', '123'),
+(8, 'm@gmail.com', 'manish', 'mmd');
 
 -- --------------------------------------------------------
 
@@ -51,19 +54,44 @@ INSERT INTO `users` (`id`, `email`, `name`, `pass`) VALUES
 -- Table structure for table `watchlist`
 --
 
-CREATE TABLE IF NOT EXISTS `watchlist` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `watchlist` (
+  `id` int(11) NOT NULL,
   `userid` int(11) NOT NULL,
-  `movieid` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `movieid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `watchlist`
+-- Indexes for dumped tables
 --
 
-INSERT INTO `watchlist` (`id`, `userid`, `movieid`) VALUES
-(1, 2, 624860);
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `watchlist`
+--
+ALTER TABLE `watchlist`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `watchlist`
+--
+ALTER TABLE `watchlist`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
